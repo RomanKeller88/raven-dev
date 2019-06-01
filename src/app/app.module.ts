993145f5412d7f1257dac25fd40ed45afd18c07f
romankeller88 <romankeller88@gmail.com>
2019-06-01 21:06:46 +0300

    Made header --- #add animation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,11 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import {HomeModule} from './pages/home/home.module';
+import {HeaderModule} from './components/header/header.module';
 
 @NgModule({
   declarations: [
@@ -14,9 +15,11 @@ import {HomeModule} from './pages/home/home.module';
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    HomeModule
+    HomeModule,
+    HeaderModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
--- src/app/pages/home/home.module.ts
+++ src/app/pages/home/home.module.ts
@@ -2,12 +2,16 @@ import {NgModule} from '@angular/core';
 import {CommonModule} from '@angular/common';
 import {HomeComponent} from './home.component';
 import {RouterModule} from '@angular/router';
+import {HeaderModule} from '../../components/header/header.module';
+import {MatTabsModule} from '@angular/material';
 
 @NgModule({
   declarations: [HomeComponent],
   imports: [
     CommonModule,
-    RouterModule
+    RouterModule,
+    HeaderModule,
+    MatTabsModule
   ]
 })
 export class HomeModule {
